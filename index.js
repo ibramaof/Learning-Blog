c9094ca85b053f73a6ed8f7d7709c7c1cef09e37
Ibrahim Abdelrahman <ibrahimaof3@gmail.com>
2026-01-12 02:09:44 +0200

    add three more posts ... hard Coded

---
+++ index.js
@@ -0,0 +1,51 @@
+// const mainEl = document.getElementById('main-el')
+// const btn = document.getElementById('btn')
+
+
+
+// btn.addEventListener('click', function () {
+//     btn.style.display = 'none'
+//     renderPost()
+// })
+
+// function renderPost() {
+//     mainEl.innerHTML +=
+//         `
+// <div class="grid-posts container3">
+//             <img src="images/article-image-01.png">
+//             <p class="date">January 10, 2026</p>
+//             <h1 class="heading">Facing JavaScript</h1>
+//             <p class="article-text">JavaScript was a major turning point. Concepts like variables, conditions, loops,
+//                 and functions felt abstract
+//                 initially, especially when combined with DOM manipulation. I often struggled to understand why things
+//                 didn’t
+//                 work as expected.</p>
+//         </div>
+        
+//         <div class="grid-posts container3">
+//             <img src="images/article-image-01.png">
+//             <p class="date">January 10, 2026</p>
+//             <h1 class="heading">Facing JavaScript</h1>
+//             <p class="article-text">JavaScript was a major turning point. Concepts like variables, conditions, loops,
+//                 and functions felt abstract
+//                 initially, especially when combined with DOM manipulation. I often struggled to understand why things
+//                 didn’t
+//                 work as expected.</p>
+//         </div>
+
+//         <div class="grid-posts container3">
+//             <img src="images/article-image-01.png">
+//             <p class="date">January 10, 2026</p>
+//             <h1 class="heading">Facing JavaScript</h1>
+//             <p class="article-text">JavaScript was a major turning point. Concepts like variables, conditions, loops,
+//                 and functions felt abstract
+//                 initially, especially when combined with DOM manipulation. I often struggled to understand why things
+//                 didn’t
+//                 work as expected.</p>
+//         </div>
+// `
+// }
+
+
+
+
